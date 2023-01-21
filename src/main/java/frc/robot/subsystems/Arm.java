0ac68821fd587f61322e51b26d7fa23f6c449a55
Brady Exoo <bradyallen67@gmail.com>
2023-01-20 19:35:13 -0500

    Add solenoid

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,6 +6,9 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -13,10 +16,13 @@ public class Arm extends SubsystemBase {
   /** Creates a new Arm. */
   WPI_TalonFX _shoulderMotor;
   WPI_TalonFX _elbowMotor;
+  Solenoid _wristSolenoid;
+  public enum wristPosition { Parallel, Perpendicular}
+
   public Arm() {
     _shoulderMotor = new WPI_TalonFX(Constants.CanIDs.ARM_SHOULDER_MOTOR_CANID);
     _elbowMotor = new WPI_TalonFX(Constants.CanIDs.ARM_ELBOW_MOTOR_CANID);
-
+    _wristSolenoid = new Solenoid(PneumaticsModuleType.REVPH, Constants.WRIST_SOLENOID_PORT);
   }
 
   public void setShoulderMotorSpeed(double speed){
@@ -25,6 +31,10 @@ public class Arm extends SubsystemBase {
 
   public void setElbowMotorSpeed(double speed){
     _elbowMotor.set(speed);
+  }
+
+  public void setWristPosition(wristPosition position){
+    _wristSolenoid.set(position == wristPosition.Parallel);
   }
 
   @Override
