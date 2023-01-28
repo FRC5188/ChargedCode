b9dc8401fed5f7537d93675ef5a58c9f4b364dac
first5188 <first5188@gmail.com>
2023-01-28 12:23:40 -0500

    idk whats going on

--- src/main/java/frc/robot/commands/CmdArmMoveToPosition.java
+++ src/main/java/frc/robot/commands/CmdArmMoveToPosition.java
@@ -5,34 +5,29 @@ import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Arm.ArmPosition;
 
 public class CmdArmMoveToPosition extends CommandBase {
-    private Arm armSubsystem;
-    private ArmPosition armPosition;
+    private Arm _armSubsystem;
+    private ArmPosition _armPosition;
 
     public CmdArmMoveToPosition(Arm armSubsystem, ArmPosition armPosition){
         // Setting the local variable to the parameter passed in.        
-        this.armSubsystem = armSubsystem;
-        this.armPosition = armPosition;
+        this._armSubsystem = armSubsystem;
+        this._armPosition = armPosition;
         // Adding arm subsystems to the requirements for this command. 
-        this.addRequirements(armSubsystem);
+        this.addRequirements(this._armSubsystem);
     }
 
     @Override
     public void initialize(){
-        armSubsystem.moveArmToPositionInit(armPosition);
+        this._armSubsystem.moveArmToPositionInit(this._armPosition);
     }
 
     @Override
     public void execute(){
-        armSubsystem.moveArmToPositionExec();
+        this._armSubsystem.moveArmToPositionExec();
     }
 
     @Override
     public boolean isFinished() {
-        return armSubsystem.isArmAtSetpoint();
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        super.end(interrupted);
+        return this._armSubsystem.isArmAtSetpoint();
     }
 }
