6eb1bc043c26dbc552195c5c3139eee246f0e112
rhit-edgingza <edgingza@rose-hulman.edu>
2023-01-31 14:12:36 -0500

    Formatted manual arm command and robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,96 +10,97 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.Button;
 import frc.robot.commands.CmdArmManual;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Arm;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final Drive _drivetrainSubsystem = new Drive();
-  private final Arm _armSubsystem = new Arm();
-  private final XboxController _driverController = new XboxController(0);
-  private final XboxController _operatorController = new XboxController(1);
+    // The robot's subsystems and commands are defined here...
+    private final Drive _driveSubsystem = new Drive();
+    private final Arm _armSubsystem = new Arm();
+    private final XboxController _driverController = new XboxController(0);
+    private final XboxController _operatorController = new XboxController(1);
 
-  private static final double ARM_MULTIPLIER = 0.1;
+    private static final double ARM_MULTIPLIER = 0.3;
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-    // Set up the default command for the drivetrain.
-    // The controls are for field-oriented driving:
-    // Left stick Y axis -> forward and backwards movement
-    // Left stick X axis -> left and right movement
-    // Right stick X axis -> rotation
-    _drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
-        _drivetrainSubsystem,
-            () -> (-modifyAxis(_driverController.getLeftY()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
-            () -> (-modifyAxis(_driverController.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
-            () -> (-modifyAxis(_driverController.getRightX()) * Drive.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)
-    ));
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Set up the default command for the drivetrain.
+        // The controls are for field-oriented driving:
+        // Left stick Y axis -> forward and backwards movement
+        // Left stick X axis -> left and right movement
+        // Right stick X axis -> rotation
+        _driveSubsystem.setDefaultCommand(new DefaultDriveCommand(
+                                               _driveSubsystem,
+                                               () -> (-modifyAxis(_driverController.getLeftY()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
+                                               () -> (-modifyAxis(_driverController.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
+                                               () -> (-modifyAxis(_driverController.getRightX()) * Drive.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)));
 
-    _armSubsystem.setDefaultCommand
-      (new CmdArmManual(
-        _armSubsystem, 
-        () -> (modifyAxis(_operatorController.getLeftX()) * ARM_MULTIPLIER), 
-        () -> (modifyAxis(_operatorController.getLeftY()) * ARM_MULTIPLIER)
-      ));
-    
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+        _armSubsystem.setDefaultCommand(new CmdArmManual(
+                                        _armSubsystem,
+                                        () -> (modifyAxis(_operatorController.getLeftX()) * ARM_MULTIPLIER),
+                                        () -> (modifyAxis(_operatorController.getLeftY()) * ARM_MULTIPLIER)));
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    // Back button zeros the gyroscope
-    // new Button(m_controller::getBackButton)
-    //         // No requirements because we don't need to interrupt anything
-    //         .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return new InstantCommand();
-  }
-
-  private static double deadband(double value, double deadband) {
-    if (Math.abs(value) > deadband) {
-      if (value > 0.0) {
-        return (value - deadband) / (1.0 - deadband);
-      } else {
-        return (value + deadband) / (1.0 - deadband);
-      }
-    } else {
-      return 0.0;
+        // Configure the button bindings
+        configureButtonBindings();
     }
-  }
 
-  private static double modifyAxis(double value) {
-    // Deadband
-    value = deadband(value, 0.05);
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        // Back button zeros the gyroscope
+        // new Button(m_controller::getBackButton)
+        // // No requirements because we don't need to interrupt anything
+        // .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+    }
 
-    // Square the axis
-    value = Math.copySign(value * value, value);
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return new InstantCommand();
+    }
 
-    return value;
-  }
+    private static double deadband(double value, double deadband) {
+        if (Math.abs(value) > deadband) {
+            if (value > 0.0) {
+                return (value - deadband) / (1.0 - deadband);
+            } else {
+                return (value + deadband) / (1.0 - deadband);
+            }
+        } else {
+            return 0.0;
+        }
+    }
+
+    private static double modifyAxis(double value) {
+        // Deadband
+        value = deadband(value, 0.05);
+
+        // Square the axis
+        value = Math.copySign(value * value, value);
+
+        return value;
+    }
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -17,10 +17,6 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.commands.CmdArmManual;
-
-
-
 
 
 public class Arm extends SubsystemBase {
