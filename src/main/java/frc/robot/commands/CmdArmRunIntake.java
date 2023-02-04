94efb1298f728f575331b26f8bbd78b28340a584
hartgl <hartgl@rose-hulman.edu>
2023-02-04 13:11:48 -0500

    add intake code for testing

--- src/main/java/frc/robot/commands/CmdArmRunIntake.java
+++ src/main/java/frc/robot/commands/CmdArmRunIntake.java
@@ -5,14 +5,20 @@
 package frc.robot.commands;
 
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
 
 public class CmdArmRunIntake extends CommandBase {
     private Arm _armSubsystem;
+    private DoubleSupplier _intakeSpeed;
 
-    public CmdArmRunIntake(Arm armSubsystem) {
+    // eventually we probably wont want to just pass in a speed. This is for testing
+    // the arm on 2/4/23
+    public CmdArmRunIntake(Arm armSubsystem, DoubleSupplier intakeSpeed) {
         this._armSubsystem = armSubsystem;
+        this._intakeSpeed = intakeSpeed;
         // Add the required subsytems.
         this.addRequirements(_armSubsystem);
     }
@@ -25,9 +31,11 @@ public class CmdArmRunIntake extends CommandBase {
     @Override
     public void execute() {
         // Set the values give from the joystick to the shoulder and elbow.
-        this._armSubsystem.setIntakeMotorSpeed(0.5);
+        this._armSubsystem.setIntakeMotorSpeed(this._intakeSpeed.getAsDouble());
         System.out.println(this._armSubsystem.getChangeInIntakeMotorCurrent());
     }
+
+
 
     @Override
     public boolean isFinished() {
