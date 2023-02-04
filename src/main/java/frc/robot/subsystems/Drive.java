39ffecd7e311609aca0db3c3555c848e62329ba2
Brady Exoo <bradyallen67@gmail.com>
2023-02-04 13:05:01 -0500

    Add match time

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -11,6 +11,7 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
@@ -231,7 +232,7 @@ public class Drive extends SubsystemBase {
     public void periodic() {
         // Convert the drive base vector into module vectors
         _visionSubsystem.getVisionEstimatedRobotPose(_odometry);
-        _odometry.updateWithTime(System.currentTimeMillis()/1000.0, getGyroscopeRotation(), new SwerveModulePosition[] {
+        _odometry.updateWithTime(Timer.getFPGATimestamp(), getGyroscopeRotation(), new SwerveModulePosition[] {
             _frontLeftModule.getModulePosition(), _frontRightModule.getModulePosition(),
             _backLeftModule.getModulePosition(), _backRightModule.getModulePosition()
         } );
