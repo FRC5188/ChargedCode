57628a154c6ac7bdb9f90825dd22fec4fe9c86a2
Pi-is-314 <tacocat2001136@gmail.com>
2023-02-11 12:51:03 -0500

    Added CANdle setLEDS for RGB vlaues and brightness

--- src/main/java/frc/robot/subsystems/CANdleLEDS.java
+++ src/main/java/frc/robot/subsystems/CANdleLEDS.java
@@ -7,6 +7,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class CANdleLEDS extends SubsystemBase {
+    public void setLEDS(short[] rgbVaue, short brightness) { //This is a set up which takes an Enumeration and a Brightness level that spits out an RGB Triplet.
+        this._candle.setLEDs(rgbVaue[0], rgbVaue[1], rgbVaue[2]);
+        this._candle.configBrightnessScalar(brightness); 
+    }
     private final CANdle _candle = new CANdle(Constants.CanIDs.CANdleID, "rio");
     
     public CANdleLEDS(){
