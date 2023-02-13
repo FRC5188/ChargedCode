ab466bb7c471f9dded668239a6451df9d5c2358d
hartgl <hartgl@rose-hulman.edu>
2023-02-13 14:47:48 -0500

    make this a command group

--- src/main/java/frc/robot/commands/CmdMoveArmToSetpoint.java
+++ src/main/java/frc/robot/commands/CmdMoveArmToSetpoint.java
@@ -5,10 +5,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Arm.ArmPosition;
 
-public class CmdMoveArmToSetpoint extends CommandBase {
+public class CmdMoveArmToSetpoint extends SequentialCommandGroup {
 
   private Arm _armSubsystem;
   private ArmPosition _setpoint;
