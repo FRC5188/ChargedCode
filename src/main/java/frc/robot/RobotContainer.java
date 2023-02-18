82615e275a10459a1fc2403928f2a8598462f4f7
Mitchell Gresham <95192428+mitchgres@users.noreply.github.com>
2023-02-18 00:48:30 -0500

    Manual control for codriver over arm w/ joystick

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,17 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.Tracer;
+
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.commands.CmdArmRunIntake;
 import frc.robot.commands.CmdArmManual;
@@ -27,44 +31,58 @@ import frc.robot.subsystems.Arm;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems and commands are defined here...
-    private final Drive _driveSubsystem = new Drive();
-    private final Arm _armSubsystem = new Arm();
-    private final XboxController _driverController = new XboxController(0);
-    
-    
-    private final XboxController _operatorController = new XboxController(1);
-    
-    private JoystickButton _operatorAButton = new JoystickButton(_operatorController, Constants.ButtonMappings.A_BUTTON);
-    private JoystickButton _operatorBButton = new JoystickButton(_operatorController, Constants.ButtonMappings.B_BUTTON);
+    // Subsystems
+    private final Drive _driveSubsystem;
+    private final Arm _armSubsystem;
+    // Controllers 
+    private final XboxController _driverController;
+    private final Joystick _operatorController;
+    // Driver Buttons
 
+    // Codriver Buttons
+    private Trigger _operatorButtonOne;
+    private Trigger _operatorButtonTwo;
 
+    // Constant Arm Multiplier In To Reduce Arm Speed
     private static final double ARM_MULTIPLIER = 0.3;
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
+        _driveSubsystem = new Drive();
+        _armSubsystem = new Arm();
         // Set up the default command for the drivetrain.
         // The controls are for field-oriented driving:
         // Left stick Y axis -> forward and backwards movement
         // Left stick X axis -> left and right movement
         // Right stick X axis -> rotation
+        _driverController = new XboxController(0);
+        _operatorController = new Joystick(1);
+        // Driver Configuration
         _driveSubsystem.setDefaultCommand(new DefaultDriveCommand(
                                                _driveSubsystem,
                                                () -> (-modifyAxis(_driverController.getLeftY()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
                                                () -> (-modifyAxis(_driverController.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
                                                () -> (-modifyAxis(_driverController.getRightX()) * Drive.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)));
-//this is a default cmd just for testing
+        // Codriver Configuration
+        // If the throttle is in one state then the Z-axis will control one motor, and when its moved then it controlled the other. 
         _armSubsystem.setDefaultCommand(new CmdArmManual(
-                                                _armSubsystem,
-                                                () -> (0.3*-modifyAxis(_operatorController.getRightY())),
-                                                () -> (0.3*-modifyAxis(_operatorController.getLeftY()))));
- 
- 
-                                            
-        // Configure the button bindings
-        configureButtonBindings();
+            _armSubsystem, 
+            () -> (getShoulderSpeed()), 
+            () -> (getArmSpeed())
+        ));
+        _operatorButtonOne = new JoystickButton(_operatorController, 1);
+        _operatorButtonTwo = new JoystickButton(_operatorController, 2);
+        _operatorButtonOne.whileTrue(new CmdArmRunIntake(_armSubsystem, () -> (0.5)));
+    }
+
+    private double getShoulderSpeed(){
+        return ARM_MULTIPLIER*((_operatorController.getThrottle() > 0) ? (0) : (_operatorController.getZ()));
+    }
+
+    private double getArmSpeed(){
+        return ARM_MULTIPLIER*((_operatorController.getThrottle() > 0) ? (_operatorController.getZ()) : (0));
     }
 
     /**
