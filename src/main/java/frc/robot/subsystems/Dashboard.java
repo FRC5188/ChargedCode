32248179aa8f08c02bef92792684bcd7638d78ef
Brady Exoo <bradyallen67@gmail.com>
2023-02-27 19:32:21 -0500

    Add some widgets

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -4,24 +4,68 @@
 
 package frc.robot.subsystems;
 
+import java.util.Map;
+
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.WidgetType;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class Dashboard extends SubsystemBase {
-  /** Creates a new Dashboard. */
-  Arm _armSubsystem;
-  Drive _driveSubsystem;
-  Vision _visionSubsystem;
 
-  public Dashboard(Arm armSubsystem, Drive driveSubsystem, Vision visionSubsystem) {
-    _armSubsystem = armSubsystem;
-    _driveSubsystem = driveSubsystem;
-    _visionSubsystem = visionSubsystem;
+  	/** Creates a new Dashboard. */
+  	Arm _armSubsystem;
+ 	Drive _driveSubsystem;
+  	Vision _visionSubsystem;
+
+  	private SendableChooser<Command> _autonomousChooser;
 
 
-  }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  	public Dashboard(Arm armSubsystem, Drive driveSubsystem, Vision visionSubsystem) {
+	  	ShuffleboardTab dashboard = Shuffleboard.getTab("Dashboard");
+	  	_armSubsystem = armSubsystem;
+	  	_driveSubsystem = driveSubsystem;
+	  	_visionSubsystem = visionSubsystem;
+
+	  	_autonomousChooser = new SendableChooser<Command>();
+
+	  	dashboard.add("Autonomous Selector", _autonomousChooser)
+	  	.withPosition(5, 4)
+	  	.withSize(5, 1)
+	  	.withWidget(BuiltInWidgets.kComboBoxChooser);
+
+		dashboard.add("Gyro", new AHRS())
+	  	.withPosition(8, 0)
+	  	.withSize(2, 4)
+	  	.withWidget(BuiltInWidgets.kGyro);
+
+
+
+  	}
+
+  	@Override
+  	public void periodic() {
+		// This method will be called once per scheduler run
+  	}
+
+  	public void addAuto(String name, Command command) {
+	  	_autonomousChooser.addOption(name, command);
+  	}
+
+  	public Command getSelectedAutonomousCommand() {
+		return _autonomousChooser.getSelected();
+  	}
+
+	public void setDefaultAuto(String name, Command command) {
+		_autonomousChooser.setDefaultOption(name, command);
+	}
 }
