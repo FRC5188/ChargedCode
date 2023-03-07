75976f165bcce28967edabb757a87ef4847101ef
Hart <hartgl@rose-hulman.edu>
2023-03-07 15:21:25 -0800

    made new auto file

---
+++ src/main/java/frc/robot/commands/GrpMoveArmAuto.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+public class GrpMoveArmAuto {
+
+    //move arm like we do in tele
+
+
+    //after moving to final position wait for arm to be in position
+
+
+    //spit out game piece after making it
+    
+}
