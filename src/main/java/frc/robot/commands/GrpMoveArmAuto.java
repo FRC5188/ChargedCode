531f126113836e488a0f644b284774f257d26fdb
garrett <hartgl@rose-hulman.edu>
2023-03-07 16:00:06 -0800

    Made a new auto routine

--- src/main/java/frc/robot/commands/GrpMoveArmAuto.java
+++ src/main/java/frc/robot/commands/GrpMoveArmAuto.java
@@ -1,13 +1,26 @@
 package frc.robot.commands;
 
-public class GrpMoveArmAuto {
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Arm.ArmPosition;
 
-    //move arm like we do in tele
-
-
-    //after moving to final position wait for arm to be in position
-
-
-    //spit out game piece after making it
+public class GrpMoveArmAuto extends SequentialCommandGroup {  
     
+  public GrpMoveArmAuto(Arm armSubsystem, ArmPosition position) {
+    this.addRequirements(armSubsystem);
+
+//
+    addCommands(
+      // Update arm goal to the intermediate position
+      new CmdArmUpdateGoal(armSubsystem, armSubsystem.getIntermediatePosition(position)),
+      // Wait for us to get there
+      new CmdArmWaitForArm(armSubsystem),
+      // Now go to actual position
+      new CmdArmUpdateGoal(armSubsystem, position),
+      // Wait for arm to reach final position
+      new CmdArmWaitForArm(armSubsystem),
+      // Turn on the intake
+      new CmdArmRunIntakeByPosition(armSubsystem, position, 0.4)
+    );
 }
+
