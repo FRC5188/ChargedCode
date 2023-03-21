b60be321575e4311e868c8265a9bc673c865c968
CONTROLS LAPTOP2 <bradyexoo@gmail.com>
2023-03-20 20:24:07 -0400

    forgot to stage some things

---
+++ src/main/java/frc/robot/test/autonomous/Autonomous.java
@@ -0,0 +1,11 @@
+package frc.robot.test.autonomous;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+@Deprecated
+public abstract class Autonomous {
+    // Runs all autonomous routines sequentially. 
+    public static Command runChecks(){
+        return null;
+    }
+}
---
+++ src/main/java/frc/robot/test/drive/Drive.java
@@ -0,0 +1,22 @@
+package frc.robot.test.drive;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+@Deprecated
+public class Drive {
+    public static Command runFowardDrivetrainCheck(){
+        return null;
+    }
+
+    public static Command runBackwardDrivetrainCheck(){
+        return null;
+    }
+
+    public static Command runRotationModuleCheck(){
+        return null;
+    }
+    /// Runs all checks for the subsystem sequentially. 
+    public static Command runChecks(){
+        return null;
+    }
+}
