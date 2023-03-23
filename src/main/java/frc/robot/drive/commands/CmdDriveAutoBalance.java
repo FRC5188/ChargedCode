f8f799ee754513b927966fb3d2b058577eb351ba
Brady Exoo <bradyallen67@gmail.com>
2023-03-23 18:55:37 -0400

    Add tolerance to AutoBalance command

--- src/main/java/frc/robot/drive/commands/CmdDriveAutoBalance.java
+++ src/main/java/frc/robot/drive/commands/CmdDriveAutoBalance.java
@@ -10,24 +10,25 @@ public class CmdDriveAutoBalance extends CommandBase {
 	private Drive _driveSubsystem;
 	private double BALANCING_MAX_SPEED;
 	private double BALANCING_MAX_ACCELERATION;
+	private final double PID_TOLERANCE;
 	private ProfiledPIDController pidController;
 	private TrapezoidProfile.Constraints BALANCING_CONSTRAINTS;
 
 	public CmdDriveAutoBalance(Drive driveSubsystem) {
 		_driveSubsystem = driveSubsystem;
+		PID_TOLERANCE = 2.0;
 		BALANCING_MAX_SPEED = Drive.MAX_VELOCITY_METERS_PER_SECOND * 0.25;
 		BALANCING_MAX_ACCELERATION = BALANCING_MAX_SPEED; //TODO: Find the actual max acceleration
 		BALANCING_CONSTRAINTS = new TrapezoidProfile.Constraints(BALANCING_MAX_SPEED, BALANCING_MAX_ACCELERATION);
 		addRequirements(_driveSubsystem);
 		pidController = new ProfiledPIDController(0.02, 0, 0, BALANCING_CONSTRAINTS); //TODO: Tune PID values
-
-
 	}
 
 
 	@Override
 	public void initialize() {
 		pidController.setGoal(0);
+		pidController.setTolerance(PID_TOLERANCE);
 	}
 
 	// Called every time the scheduler runs while the command is scheduled.
