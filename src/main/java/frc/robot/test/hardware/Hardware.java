c8add1b65f1919e67577db7e7607be194ec944d8
CONTROLS LAPTOP2 <bradyexoo@gmail.com>
2023-03-24 20:29:08 -0400

    custom library for outputting color to termial :D

--- src/main/java/frc/robot/test/hardware/Hardware.java
+++ src/main/java/frc/robot/test/hardware/Hardware.java
@@ -1,14 +1,20 @@
 package frc.robot.test.hardware;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Robot;
+import frc.robot.test.console_output.Output;
 
 @Deprecated
 public abstract class Hardware {
-    public static Command runRoboRIOChecks(){
-        return null;
+
+    public static Command check(){
+        return null;       
     }
-    /// Checks if the battery voltage is sufficet. (12-13V)
-    public static Command runBatteryChecks(){
-        return null;
-    }
+
+    private static boolean isCANBusConnected(){return RobotController.getCANStatus().percentBusUtilization <= 0;}
+
+    private static boolean isBatteryCharged(){return (RobotController.getBatteryVoltage() < 10) ? (true): (false);}
+
 }
